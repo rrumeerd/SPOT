5541ddb1c0e36cf2edf82b0d0501b381cf68ee5c
rrumeerd <rrumeerd@gmail.com>
2025-10-23 15:07:27 -0500

    db connect

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,10 @@
 const mysql = require('mysql');
 const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'spot',
+    host: 'b1hukuz0kz2dwd5s0iiv-mysql.services.clever-cloud.com',
+    user: 'ui1kb6yusr15p6xe',
+    database: 'b1hukuz0kz2dwd5s0iiv',
+    password: 'sFxhFXXCdKEcbmB5TPrc',
+    multipleStatements: true
 });
 
 mysqlConnection.connect(function(err){
