1d04e86e1d19dda27f6594848c431a8ba4712264
DREEMURR <rrumeerd@gmail.com>
2025-10-28 19:58:58 -0500

    Add alias and marca fields to vehiculo routes

--- backend/routes/vehiculos.js
+++ backend/routes/vehiculos.js
@@ -27,10 +27,10 @@ router.get('/user/:ID_user', (req, res) =>{
 });
 
 router.post('/', (req, res) => {
-    const { ID_usuario, Matricula, Modelo, Tipo_vehiculo, year} = req.body;
+    const { ID_usuario, Matricula, Modelo, Tipo_vehiculo, year, alias, marca} = req.body;
     mysqlConnection.query(
-        'INSERT INTO vehiculo (ID_usuario, Matricula, Modelo, Tipo_vehiculo, year) VALUES (?, ?, ?, ?, ? )',
-        [ID_usuario, Matricula, Modelo, Tipo_vehiculo, year ],
+        'INSERT INTO vehiculo (ID_usuario, Matricula, Modelo, Tipo_vehiculo, year, alias, marca) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        [ID_usuario, Matricula, Modelo, Tipo_vehiculo, year, alias, marca],
         (err) => {
             if (err) {
                 console.error('Error al agregar vehiculo', err);
@@ -44,9 +44,9 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
     const { id } = req.params;
-    const {Modelo, Tipo_vehiculo, year, Matricula} = req.body;
+    const {Matricula, Modelo, Tipo_vehiculo, year, alias, marca} = req.body;
     mysqlConnection.query(
-        'UPDATE vehiculo SET Modelo=?, Tipo_vehiculo=?, year=? WHERE id=?',
+        'UPDATE vehiculo SET Modelo=?, Tipo_vehiculo=?, year=?, alias=?, marca=?, Matricula=? WHERE id=?',
         [Modelo, Tipo_vehiculo, year, id],
         (err) => {
             if (err) {
